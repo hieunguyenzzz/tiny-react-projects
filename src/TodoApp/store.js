178977363bb54706e65b0c876db6fc1942b94964
Hieu Nguyen <hieunguyenel@gmail.com>
2019-01-30 10:24:50 +0700

    TodoApp: Learn how to use store.subscribe() to efficiently persist some of the app’s state to localStorage and restore it after a refresh.

--- src/TodoApp/store.js
+++ src/TodoApp/store.js
@@ -1,7 +1,15 @@
 import {createStore} from 'redux';
 import RootReducer from './reducers'
+import {loadState, saveState} from "./localStorage";
 
-const store = createStore(RootReducer);
+const persistedState = loadState();
+const store = createStore(RootReducer, {
+    todos: persistedState.todos
+});
+
+store.subscribe(() => {
+    saveState(store.getState());
+})
 
 export default store;
 
