4a967b42b334598c2564706d07401e08625c8014
Hieu Nguyen <hieunguyenel@gmail.com>
2019-01-30 12:05:38 +0700

    TodoApp: wrap the saveState in throttle function to avoid querying JSON.stringify too often

--- src/TodoApp/store.js
+++ src/TodoApp/store.js
@@ -1,15 +1,16 @@
 import {createStore} from 'redux';
 import RootReducer from './reducers'
 import {loadState, saveState} from "./localStorage";
+import throttle from 'lodash/throttle';
 
 const persistedState = loadState();
 const store = createStore(RootReducer, {
     todos: persistedState.todos
 });
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
     saveState(store.getState());
-})
+}, 1000))
 
 export default store;
 
