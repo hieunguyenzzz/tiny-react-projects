82051d413789a4b3b518626c9b5fd32b820e75fa
Hieu Nguyen <hieunguyenel@gmail.com>
2019-02-01 16:12:32 +0700

    TodoApp:  add middleware implementation

--- src/TodoApp/store.js
+++ src/TodoApp/store.js
@@ -1,35 +1,36 @@
 import {createStore} from 'redux';
-import fakeBackend from './api/index';
 import RootReducer from './reducer/index'
-import {SHOW_ALL} from "./instant";
 
-const dispatchWithLog = (store) => {
-    const rawDispatch = store.dispatch;
-    return (action) => {
-        console.log('prev state', store.getState());
-        const nextState = rawDispatch(action);
-        console.log('action', action);
-        console.log('after state', store.getState());
-        return nextState;
+const dispatchWithLog = (store) => (next) => (action) => {
+    console.log('prev state', store.getState());
+    const nextState = next(action);
+    console.log('action', action);
+    console.log('after state', store.getState());
+    return nextState;
+}
+
+const dispatchWithPomiseAction = (store) => (next) => (action) => {
+    if (typeof action.then === 'function') {
+        return action.then(promiseAction => next(promiseAction));
+    } else {
+        return next(action);
     }
 }
 
-const dispatchWithPomiseAction = (store) => {
-    const rawDispatch = store.dispatch;
-    return (action) => {
-        if (typeof action.then === 'function') {
-            return action.then(promiseAction => rawDispatch(promiseAction));
-        } else {
-            return rawDispatch(action);
-        }
-    }
+const applyMiddleware = (store, middlewares) => {
+    middlewares.forEach((middleware) => {
+        store.dispatch = middleware(store)(store.dispatch);
+    })
 }
 
 const configureStore = () => {
     const store = createStore(RootReducer);
+    const middleWares = [];
 
-    store.dispatch = dispatchWithLog(store);
-    store.dispatch = dispatchWithPomiseAction(store);
+    middleWares.push(dispatchWithLog);
+    middleWares.push(dispatchWithPomiseAction);
+    applyMiddleware(store, middleWares);
+
     return store;
 
 }
